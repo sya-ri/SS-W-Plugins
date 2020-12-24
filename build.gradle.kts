ea9e300e11914232d90bc9a405fef3c3ed7c32e7
sya-ri <sya79lua@gmail.com>
2020-12-24 15:33:14 +0900

    [*] プラグインファイルがバンジーになってない問題を修正

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.4.21"
     id("com.github.johnrengelman.shadow") version "6.1.0" apply false
-    id("net.minecrell.plugin-yml.bukkit") version "0.3.0" apply false
+    id("net.minecrell.plugin-yml.bungee") version "0.3.0" apply false
 }
 
 allprojects {
@@ -16,7 +16,7 @@ allprojects {
 subprojects {
     apply(plugin = "kotlin")
     apply(plugin = "com.github.johnrengelman.shadow")
-    apply(plugin = "net.minecrell.plugin-yml.bukkit")
+    apply(plugin = "net.minecrell.plugin-yml.bungee")
 
     repositories {
         maven {
