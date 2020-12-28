f3b2f8f4d5ba5b7ba7ff4249887e10a787ca0b3b
sya-ri <sya79lua@gmail.com>
2020-12-28 12:46:13 +0900

    [Chat] グローバルチャットができるように変更

---
+++ SS-W-Chat/src/main/kotlin/com/github/syari/ss/wplugins/chat/ChatSender.kt
@@ -0,0 +1,34 @@
+package com.github.syari.ss.wplugins.chat
+
+import com.github.syari.ss.wplugins.chat.converter.MessageConverter
+import com.github.syari.ss.wplugins.core.code.StringEditor.toColor
+import com.github.syari.ss.wplugins.core.code.StringEditor.toUncolor
+import com.github.syari.ss.wplugins.core.message.JsonBuilder
+import com.github.syari.ss.wplugins.core.message.JsonBuilder.Companion.buildJson
+import net.md_5.bungee.api.chat.TextComponent
+import net.md_5.bungee.api.connection.ProxiedPlayer
+import java.util.UUID
+
+class ChatSender {
+    var isGlobalChannel = false
+
+    fun getGlobalTemplateMessage(player: ProxiedPlayer, message: String): TextComponent {
+        val name = player.displayName
+        val serverName = player.server.info.name
+        return buildJson {
+            append(
+                "&b$name".toColor, JsonBuilder.Hover.Text("&bServer: &f$serverName")
+            )
+            append("&b: ")
+            append(MessageConverter.convert(message.toUncolor).formatMessage)
+        }
+    }
+
+    companion object {
+        private val list = mutableMapOf<UUID, ChatSender>()
+
+        fun get(player: ProxiedPlayer): ChatSender {
+            return list.getOrPut(player.uniqueId) { ChatSender() }
+        }
+    }
+}
