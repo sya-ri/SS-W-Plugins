d8a77221699153e002071541638b703eeadf5bf3
sya-ri <sya79lua@gmail.com>
2020-12-28 20:50:56 +0900

    [Core] プラグインメッセージの受信が行われないバグを修正

--- SS-W-Core/src/main/kotlin/com/github/syari/ss/wplugins/core/Main.kt
+++ SS-W-Core/src/main/kotlin/com/github/syari/ss/wplugins/core/Main.kt
@@ -1,6 +1,7 @@
 package com.github.syari.ss.wplugins.core
 
 import com.github.syari.ss.wplugins.core.code.SSPlugin
+import com.github.syari.ss.wplugins.core.pluginMessage.PluginMessage
 import net.md_5.bungee.api.CommandSender
 import net.md_5.bungee.api.plugin.Plugin
 
@@ -14,8 +15,17 @@ class Main: SSPlugin() {
         lateinit var console: CommandSender
     }
 
+    override val listeners = listOf(
+        PluginMessage
+    )
+
+    override val onEnables = listOf(
+        PluginMessage
+    )
+
     override fun onEnable() {
         plugin = this
         console = proxy.console
+        runOnEnable()
     }
 }
