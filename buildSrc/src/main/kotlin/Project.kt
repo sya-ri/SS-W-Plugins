623b6e76c2386e125ed1c469e86449c4c9f7c7c6
sya-ri <sya79lua@gmail.com>
2020-12-30 00:03:09 +0900

    [Chat] メッセージが正常に処理されないバグを修正

--- buildSrc/src/main/kotlin/Project.kt
+++ buildSrc/src/main/kotlin/Project.kt
@@ -24,7 +24,7 @@ interface Project {
 
     object Chat: Project {
         override val name = "SS-W-Chat"
-        override val version = build(1)
+        override val version = build(2)
         override val group = subgroup("chat")
         override val dependProject = listOf(Core)
     }
