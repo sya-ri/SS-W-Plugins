51271b17cd1e88a1af7966f475ab0ea161d1685e
sya-ri <sya79lua@gmail.com>
2020-12-29 14:21:48 +0900

    [Core+1] MessageTemplateのパッケージを変更

--- SS-W-Core/src/main/kotlin/com/github/syari/ss/wplugins/core/pluginMessage/SSPluginMessageEvent.kt
+++ SS-W-Core/src/main/kotlin/com/github/syari/ss/wplugins/core/pluginMessage/SSPluginMessageEvent.kt
@@ -1,6 +1,6 @@
 package com.github.syari.ss.wplugins.core.pluginMessage
 
+import com.github.syari.ss.template.message.PluginMessageTemplate
 import com.github.syari.ss.wplugins.core.event.CustomEvent
-import com.github.syari.ss.wplugins.core.pluginMessage.template.PluginMessageTemplate
 
 class SSPluginMessageEvent(val template: PluginMessageTemplate): CustomEvent()
