c8ee16b68917e9eaebcd5afd0a07701ba63463db
sya-ri <sya79lua@gmail.com>
2020-12-31 20:54:52 +0900

    [AccessBlocker] ホワイトリストを追加

--- SS-W-AccessBlocker/src/main/kotlin/com/github/syari/ss/wplugins/accessblocker/ConfigLoader.kt
+++ SS-W-AccessBlocker/src/main/kotlin/com/github/syari/ss/wplugins/accessblocker/ConfigLoader.kt
@@ -6,6 +6,7 @@ import com.github.syari.ss.wplugins.core.code.OnEnable
 import com.github.syari.ss.wplugins.core.config.CreateConfig.config
 import com.github.syari.ss.wplugins.core.config.dataType.ConfigDataType
 import net.md_5.bungee.api.CommandSender
+import java.util.UUID
 
 object ConfigLoader: OnEnable {
     override fun onEnable() {
@@ -15,6 +16,9 @@ object ConfigLoader: OnEnable {
     fun load(sender: CommandSender) {
         config(plugin, sender, "config.yml") {
             ModList.availableList = get("mod", ConfigDataType.STRINGLIST, listOf(), false)
+            WhiteList.isEnable = get("whitelist.enable", ConfigDataType.BOOLEAN, default = true, false)
+            val uuidList = get("whitelist.uuid", ConfigDataType.STRINGLIST, listOf(), false)
+            WhiteList.list = uuidList.map { UUID.fromString(it) }.toMutableList()
         }
     }
 }
