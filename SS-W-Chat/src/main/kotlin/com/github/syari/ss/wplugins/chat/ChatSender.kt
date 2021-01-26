8f5029f974eeb1c5abd664a20560068e6b4ad383
sya-ri <sya79lua@gmail.com>
2021-01-26 15:19:44 +0900

    [Core] UUIDPlayerを追加

--- SS-W-Chat/src/main/kotlin/com/github/syari/ss/wplugins/chat/ChatSender.kt
+++ SS-W-Chat/src/main/kotlin/com/github/syari/ss/wplugins/chat/ChatSender.kt
@@ -1,21 +1,19 @@
 package com.github.syari.ss.wplugins.chat
 
-import com.github.syari.ss.wplugins.chat.Main.Companion.plugin
 import com.github.syari.ss.wplugins.chat.channel.ChatChannel
+import com.github.syari.ss.wplugins.core.player.UUIDPlayer
 import net.md_5.bungee.api.connection.ProxiedPlayer
-import java.util.UUID
 
-class ChatSender(private val uuid: UUID) {
+class ChatSender(private val uuidPlayer: UUIDPlayer) {
     var channel: ChatChannel = ChatChannel.Global
 
-    val player: ProxiedPlayer?
-        get() = plugin.proxy.getPlayer(uuid)
+    val player by uuidPlayer::player
 
     companion object {
-        private val list = mutableMapOf<UUID, ChatSender>()
+        private val list = mutableMapOf<UUIDPlayer, ChatSender>()
 
-        fun get(player: ProxiedPlayer) = get(player.uniqueId)
+        fun get(player: ProxiedPlayer) = get(UUIDPlayer(player))
 
-        fun get(uuid: UUID) = list.getOrPut(uuid) { ChatSender(uuid) }
+        fun get(uuidPlayer: UUIDPlayer) = list.getOrPut(uuidPlayer) { ChatSender(uuidPlayer) }
     }
 }
--- SS-W-Chat/src/main/kotlin/com/github/syari/ss/wplugins/chat/ConfigLoader.kt
+++ SS-W-Chat/src/main/kotlin/com/github/syari/ss/wplugins/chat/ConfigLoader.kt
@@ -9,6 +9,7 @@ import com.github.syari.ss.wplugins.core.Main.Companion.console
 import com.github.syari.ss.wplugins.core.code.OnEnable
 import com.github.syari.ss.wplugins.core.config.CreateConfig.config
 import com.github.syari.ss.wplugins.core.config.dataType.ConfigDataType
+import com.github.syari.ss.wplugins.core.player.UUIDPlayer
 import net.md_5.bungee.api.CommandSender
 
 object ConfigLoader : OnEnable {
@@ -25,7 +26,7 @@ object ConfigLoader : OnEnable {
                 val templateDiscord = get("channel.$it.template.discord", ChatTemplate.ConfigDataType, ChatTemplate.Discord, false)
                 val prefix = get("channel.$it.prefix", ConfigDataType.STRING, false)
                 val players = get("channel.$it.player", ConfigDataType.UUIDLIST, false).orEmpty()
-                regex to ChatChannelOption(discordChannelId, templateDiscord, prefix, players)
+                regex to ChatChannelOption(discordChannelId, templateDiscord, prefix, players.map(::UUIDPlayer))
             }.orEmpty()
             Discord.joinUrl = get("discord.url", ConfigDataType.STRING, false)
             Discord.listenChannels = buildMap {
