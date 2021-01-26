01b07fe0bbf3ed8081fbda9ad34098b00e96fc51
sya-ri <sya79lua@gmail.com>
2021-01-27 04:18:52 +0900

    [GlobalPlayers] プレイヤーリストにヘッダーフッターを追加

--- SS-W-GlobalPlayers/src/main/kotlin/com/github/syari/ss/wplugins/globalplayers/TabUpdater.kt
+++ SS-W-GlobalPlayers/src/main/kotlin/com/github/syari/ss/wplugins/globalplayers/TabUpdater.kt
@@ -1,6 +1,7 @@
 package com.github.syari.ss.wplugins.globalplayers
 
 import com.github.syari.ss.template.message.PluginMessageTemplateTabList
+import com.github.syari.ss.wplugins.core.message.JsonBuilder.Companion.buildJson
 import com.github.syari.ss.wplugins.core.pluginMessage.PluginMessage
 import com.github.syari.ss.wplugins.core.scheduler.CreateScheduler.runLater
 import com.github.syari.ss.wplugins.globalplayers.Main.Companion.plugin
@@ -13,9 +14,24 @@ import net.md_5.bungee.event.EventHandler
 object TabUpdater : Listener {
     private fun updateTabList() {
         plugin.runLater(5) {
-            val playerList = plugin.proxy.players.map(ProxiedPlayer::getDisplayName)
+            val playerList = plugin.proxy.players
+            val playerNameList = playerList.map(ProxiedPlayer::getDisplayName)
             plugin.proxy.serversCopy.values.forEach {
-                PluginMessage.send(it, PluginMessageTemplateTabList(playerList))
+                PluginMessage.send(it, PluginMessageTemplateTabList(playerNameList))
+            }
+            val header = buildJson {
+                appendln("&8&l>> &6&lSecond Story Server &8&l<<")
+                appendln()
+                appendln("&6&lss-rpg.net")
+                append("    &7&l&m---------------------------&r    ")
+            }
+            val footer = buildJson {
+                appendln("    &7&l&m---------------------------&r    ")
+                appendln()
+                appendln("&6&l${playerList.size} / &6&l${plugin.proxy.config.listeners.first().maxPlayers}")
+            }
+            playerList.forEach {
+                it.setTabHeader(header, footer)
             }
         }
     }
